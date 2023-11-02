67341ab121cdae28959eefe7c79cbfe3329ceb04
rod <rodrip0129@gmail.com>
2023-11-02 07:48:33 -0300

    fix all linters

--- db/migrate/20231102095855_create_messages.rb
+++ db/migrate/20231102095855_create_messages.rb
@@ -1,8 +1,5 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
-    create_table :messages do |t|
-
-      t.timestamps
-    end
+    create_table :messages, &:timestamps
   end
 end
