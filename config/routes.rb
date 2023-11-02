f2f270f4fbe472e8430860cef635e6bde3412923
rod <rodrip0129@gmail.com>
2023-11-02 09:24:33 -0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'root#index'
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     get '/greetings/random', to: 'greetings#random'
   end
 end
